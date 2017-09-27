5591a8616ad4794234a212bed84c2496ce1c0fb8
twchuck1 <32159827+twchuck@users.noreply.github.com>
2017-09-27 10:57:42 +0800

    add rebase2

---
+++ src/main/CalculatorRebase2.java
@@ -0,0 +1,20 @@
+package main;
+
+public class CalculatorRebase2 {
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static int sub(int a, int b) {
+		return a - b - b - b - b;
+	}
+
+	public static int multiply(int a, int b) {
+		return a * b * b;
+	}
+
+	public static int div(int a, int b) {
+		return a / b / b;
+	}
+}
